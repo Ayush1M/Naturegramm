38c9065858e1feb0ebe7d766fc4be7b8afbcf629
Ayush1M <worklife2442@gmail.com>
2023-04-12 00:43:10 +0530

    Added like functionality.

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 import posts from './data.js'
 
 
+document.addEventListener("click", (e)=> {
+    if(e.target.dataset.like){
+        handleLikeClick(e.target.dataset.like)
+    }
+})
+
+function handleLikeClick(postId){
+    const targetLikeObj = posts.filter((post) => post.uuid === postId)[0]
+    
+    if(targetLikeObj.isLiked){
+        targetLikeObj.likes--
+        targetLikeObj.isLiked = false
+    }else{
+        targetLikeObj.likes++
+        targetLikeObj.isLiked = true
+    }
+    render()
+}
+
+
 function getPostHtml(){
 
     let postHtml = ``
 
     posts.forEach((post) =>{
 
-        postHtml+= `
+        const likeIcon = post.isLiked ? "liked" : ""
+
+        postHtml += `
         
         <div class="post">
         <div class="post-header">
@@ -19,15 +41,14 @@ function getPostHtml(){
         </div>
         <img src="${post.post}" alt="" class="post-img">
         <div class="icon-container">
-        <i class="fa-regular fa-heart"></i>
+        <i class="fa-solid fa-heart ${likeIcon}" data-like = ${post.uuid}></i>
         <i class="fa-regular fa-comment"></i>
-        <img>
+        <i class="fa-regular fa-paper-plane"></i>
         </div>
         <p class="likes">${post.likes} likes</p>
         <p class="description"> <strong>${post.name}</strong>  ${post.comment}</p>
         </div>
         `
-        
     })
     return postHtml
 }
@@ -36,9 +57,6 @@ function getPostHtml(){
 function render(){
     document.getElementById("container").innerHTML = getPostHtml()
 }
-
 render()
-
-
 
 
